3c7b8ed8efa2136d610034aa0a95d162d9b5093b
Tariq Ali <tarikali7444@gmail.com>
2024-09-09 22:36:04 +0530

    chnages

--- src/google marzipano master demos-embedded-hotspots/index.js
+++ src/google marzipano master demos-embedded-hotspots/index.js
@@ -16,7 +16,7 @@
 'use strict';
 
 // Create viewer.
-var viewer = new Marzipano.Viewer(document.getElementById('pano'));
+var viewer = new Marzipano.Viewer(document.getElementById('pano'));  
     var urlPrefix = "tiles";
 
 // Create source.
@@ -65,7 +65,7 @@ container.createHotspot(document.getElementById('iframeselect'), { yaw: -0.35, p
 var hotspotHtml = {
   youtube: '<iframe  width="990" height="350" src="https://www.youtube.com/embed/N9KXh34IT94?si=WWoWSqhutuy3bE1P;" frameborder="0" ></iframe>',
   youtubeWithControls: '<iframe id="youtubeWithControls" width="990" height="350" src="https://www.youtube.com/embed/tY8aNLzUUJw?si=hLk8wwhSuesDOU8g" frameborder="0" allowfullscreen></iframe>',
-  googleMaps: '<iframe id="googlemaps" width="1280" height="480" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9487.563699358636!2d-9.211273541013671!3d38.69789785451112!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd1ecf578f4d20e9%3A0x530952e38d140ae6!2sDigisfera+-+Fotografia+e+Inform%C3%A1tica%2C+Lda!5e1!3m2!1spt-PT!2spt!4v1429956174252" width="600" height="450" frameborder="0" style="border:0"></iframe>',
+  googleMaps: '<iframe id="googlemaps" width="990" height="350"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9487.563699358636!2d-9.211273541013671!3d38.69789785451112!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd1ecf578f4d20e9%3A0x530952e38d140ae6!2sDigisfera+-+Fotografia+e+Inform%C3%A1tica%2C+Lda!5e1!3m2!1spt-PT!2spt!4v1429956174252" width="600" height="450" frameborder="0" style="border:0"></iframe>',
   koloreyes: '<iframe id="koloreyes" src="https://eyes.kolor.com/video/i/kolor/54ef73cbaaa38b2943c8a7d72a4b00e6" type="text/html" width="1280" height="480" frameborder="0" scrolling="no" allowfullscreen="true"> </iframe>'
 };
 
